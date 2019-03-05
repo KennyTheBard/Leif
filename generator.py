19518c619c71b8983421a457b2de26a9050acbad
KennyTheBard <octav_teculescu@yahoo.com>
2019-03-05 14:25:51 +0200

    Added smoothening support

--- generator.py
+++ generator.py
@@ -2,14 +2,6 @@ import random
 import sys
 
 
-def print_world_map(world):
-	for i in range(len(world) - 1):
-		for j in range(len(world[i]) - 1):
-			sys.stdout.write(world[i][j])
-		sys.stdout.write('\n')
-	sys.stdout.write('\n')
-
-
 WATER = ' '
 GRASS = '^'
 SAND = '.'
@@ -23,7 +15,54 @@ terrain = [WATER, GRASS, SAND, TALLGRASS, GRAVEL, STONE, BUSHES]
 objects = ['Y', 'Q', '@', '&', '#']
 
 width = 100
-height = 75
+height = 35
+
+
+def print_world_map(world):
+	for i in range(len(world) - 1):
+		for j in range(len(world[i]) - 1):
+			sys.stdout.write(world[i][j])
+		sys.stdout.write('\n')
+	sys.stdout.write('\n')
+
+
+def get_neighbours(pos, world):
+	x = pos[0]
+	y = pos[1]
+	neigh = []
+
+	for i in range(y - 1, y + 2):
+		for j in range(x - 1, x + 2):
+			if i >= 0 and i < len(world) and j >= 0 and j < len(world[0]):
+				neigh.append(world[i][j])
+	return neigh
+
+
+def set_counters_zero(counters):
+	for i in range(len(terrain)):
+		counters[terrain[i]] = 0
+
+
+def smoothen(world):
+	counters = {}
+
+	for i in range(len(world)):
+		for j in range(len(world[0])):
+			set_counters_zero(counters)
+			neigh = get_neighbours((j, i), world)
+			for n in neigh:
+				counters[n] += 1
+
+			terr = terrain[1]
+			max = 0
+			for t in range(len(terrain)):
+				if counters[terrain[t]] > max:
+					max = counters[terrain[t]]
+					terr = terrain[t]
+
+
+			world[i][j] = terr
+
 
 for i in range(0, height - 1):
 	world.append([])
@@ -31,11 +70,11 @@ for i in range(0, height - 1):
 		world[i].append('X')
 
 zones = []
-num_zones = random.randint(50, 100)
+num_zones = random.randint(30, 60)
 for i in range(num_zones):
 	x = random.randint(10, width - 11)
 	y = random.randint(10, height - 11)
-	type = random.randint(0, len(terrain) - 1)
+	type = random.randint(1, len(terrain) - 1)
 	zones.append((x, y, terrain[type]))
 
 for i in range(0, width - 1, 20):
@@ -59,5 +98,10 @@ for i in range(0, height - 1):
 				min_val = zone[2]
 
 		world[i][j] = min_val
+
+print_world_map(world)
+
+for i in range(10):
+	smoothen(world)
 
 print_world_map(world)
